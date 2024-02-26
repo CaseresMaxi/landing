701ebd622253c24a2aafe810cc58314d6f414462
Caseres <maxy_caseres4@hotmail.com>
2024-02-26 11:52:30 -0300

    hot fix

--- src/sections/XpSection/XpSection.tsx
+++ src/sections/XpSection/XpSection.tsx
@@ -9,7 +9,7 @@ const XpSection = () => {
         <>
             <div className="xp-section-wrapper">
                 <Xp xp={[{
-                    title: 'Ingeniero de Software', date: '9/2022 | 3/2024', name: 'Pickit', desc: `
+                    title: 'Ingeniero de Software', date: '9/2022 | Hoy', name: 'Pickit', desc: `
                     En Pickit, me encargué del mantenimiento de la aplicación de back office, focalizando mis esfuerzos exclusivamente en el desarrollo front-end con React. Gracias a mi sólido desempeño, se me confió la gestión de un nuevo proyecto que lideré hasta su lanzamiento en los mercados de Argentina, México y Colombia. Este proyecto consistió en el desarrollo de una aplicación de administración para un sistema de entregas a domicilio, similar a Pedidos Ya. Durante esta experiencia, tuve a mi cargo un equipo de colegas y pude concentrarme aún más en el desarrollo front-end. Además, desempeñé un papel fundamental en la creación y colaboración activa en la implementación de una arquitectura de micro front-end para los proyectos clave de la empresa. Posteriormente, participé en otros proyectos, incluyendo la creación de una biblioteca de componentes propia y la definición de estándares de calidad para futuros proyectos de la empresa.`,
                     tecs: ["React", "TypeScript", "Sass", "Redux", "Saga", "Story book", "Zustan", "Gsap", "Jira", "Figma"],
                     link: "https://pickit.com.ar/"
